2f531ee6fd8dac2e5c21d2499a1cfd41bb0740fa
fmelods <felipemelodesousa4@gmail.com>
2024-08-20 22:08:34 -0300

    Passando função por PROPS.

--- aula05/src/components/Cabecalho/Cabecalho.tsx
+++ aula05/src/components/Cabecalho/Cabecalho.tsx
@@ -1,7 +1,11 @@
-function Cabecalho(props:{tituloProps:string}){
+function Cabecalho(props:{tituloProps:string,avisoProps:Function}){
+
     return(
         <header>
             <h1>{props.tituloProps}</h1>
+            <div>
+                <button onClick={()=> props.avisoProps()}>Aviso</button>
+            </div>
         </header>
     );
 }
