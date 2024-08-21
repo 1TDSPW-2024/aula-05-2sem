2d6738e414af72d543766cb6e937b02f207cac8b
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2024-08-20 22:01:35 -0300

    Props sendo passada por variável...

--- aula05/src/App.tsx
+++ aula05/src/App.tsx
@@ -1,7 +1,14 @@
+import Cabecalho from "./components/Cabecalho/Cabecalho";
+
 function App(){
+    const titulo:string = "Minha página!";
+
+    const aviso = ()=> alert(titulo);
+
   return (
     <div>
       <h1>Componente App</h1>
+      <Cabecalho tituloProps={titulo} avisoProps={aviso}/>
     </div>
     );
 }
---
+++ aula05/src/components/Cabecalho/Cabecalho.tsx
@@ -0,0 +1,9 @@
+function Cabecalho(props:{tituloProps:string}){
+    return(
+        <header>
+            <h1>{props.tituloProps}</h1>
+        </header>
+    );
+}
+export default Cabecalho;
+ 
