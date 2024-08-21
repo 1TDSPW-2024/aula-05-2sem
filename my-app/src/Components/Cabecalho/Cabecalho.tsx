aa88a9cc48532707601be742a56b59767db454b6
Andenik <andenik@yahoo.com.br>
2024-08-20 22:17:22 -0300

    desestruturando a props

--- my-app/src/Components/Cabecalho/Cabecalho.tsx
+++ my-app/src/Components/Cabecalho/Cabecalho.tsx
@@ -1,7 +1,13 @@
-function Cabecalho(props: { tituloProps: string }) {
+function Cabecalho(props: { tituloProps: string; avisoProps: () => void }) {
+  // addEventListener("click", () => {});
+  document.title = tituloProps;
   return (
     <header>
       <h1>{props.tituloProps}</h1>
+      <div>
+        <button onClick={() => props.avisoProps()}>Aviso</button>
+      </div>
+      <div></div>
     </header>
   );
 }
