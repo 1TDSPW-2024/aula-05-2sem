7046352c653bc7fc19272b9cc20ea03fd1f57a62
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2024-08-20 22:17:07 -0300

    Desestruturando a PROPS...

--- aula05/src/components/Cabecalho/Cabecalho.tsx
+++ aula05/src/components/Cabecalho/Cabecalho.tsx
@@ -1,11 +1,12 @@
-function Cabecalho(props:{tituloProps:string,avisoProps:Function}){
+function Cabecalho({tituloProps,avisoProps}:{tituloProps:string,avisoProps:Function}){
 
-       
+        document.title = tituloProps;
+  
     return(
         <header>
-            <h1>{props.tituloProps}</h1>
+            <h1>{tituloProps}</h1>
             <div>
-                <button onClick={()=> props.avisoProps()}>Aviso</button>
+                <button onClick={()=> avisoProps()}>Aviso</button>
             </div>
         </header>
     );
